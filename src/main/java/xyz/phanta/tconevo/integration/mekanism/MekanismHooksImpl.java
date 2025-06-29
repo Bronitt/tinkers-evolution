5c78aee3e1829f3470267e82c53d53aa9d563005
Bronit <suslinv037@gmail.com>
2025-06-30 01:35:46 +0300

    MekaCEU patch

--- src/main/java/xyz/phanta/tconevo/integration/mekanism/MekanismHooksImpl.java
+++ src/main/java/xyz/phanta/tconevo/integration/mekanism/MekanismHooksImpl.java
@@ -1,7 +1,7 @@
 package xyz.phanta.tconevo.integration.mekanism;
 
 import mekanism.common.MekanismItems;
-import mekanism.common.item.ItemHDPE;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.oredict.OreDictionary;
@@ -14,14 +14,14 @@ public class MekanismHooksImpl implements MekanismHooks {
 
     @Override
     public void onInit(FMLInitializationEvent event) {
-        OreDictionary.registerOre("pelletHDPE", createHdpeStack(ItemHDPE.PlasticItem.PELLET));
-        OreDictionary.registerOre("rodHDPE", createHdpeStack(ItemHDPE.PlasticItem.ROD));
-        OreDictionary.registerOre("sheetHDPE", createHdpeStack(ItemHDPE.PlasticItem.SHEET));
-        OreDictionary.registerOre("stickHDPE", createHdpeStack(ItemHDPE.PlasticItem.STICK));
+        OreDictionary.registerOre("pelletHDPE", createHdpeStack(MekanismItems.HDPE_PELLET));
+        OreDictionary.registerOre("rodHDPE", createHdpeStack(MekanismItems.HDPE_ROD));
+        OreDictionary.registerOre("sheetHDPE", createHdpeStack(MekanismItems.HDPE_SHEET));
+        OreDictionary.registerOre("stickHDPE", createHdpeStack(MekanismItems.HDPE_STICK));
     }
 
-    private static ItemStack createHdpeStack(ItemHDPE.PlasticItem type) {
-        return new ItemStack(MekanismItems.Polyethene, 1, type.ordinal());
+    private static ItemStack createHdpeStack(Item item) {
+        return new ItemStack(item, 1);
     }
 
     @Override
