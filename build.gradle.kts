0218ad23f3fd99f37dcf51dba83c341969df855f
Bronit <suslinv037@gmail.com>
2025-06-30 04:19:58 +0300

    Update package name and git branch. Release 1.0.50

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,7 +62,7 @@ group = props.modPackage
 version = props.modVersion
 
 base {
-    archivesName = withProps { "$modId-$mcVersion" }
+    archivesName = withProps { "$modId-$mcVersion-MekaCEU" }
 }
 
 /*
